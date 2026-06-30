38252900f32db23c8da66d093357b481f44197a1
georgeabro-collab <george.abro@mjwholesale.com>
2026-06-30 15:59:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from "express";
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => res.send("dtf-ai-service is running"));
-
-app.listen(port, () => console.log(`Listening on ${port}`));
+const ORIGIN = "https://synergy-print-solutions.myshopify.com";
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", ORIGIN);
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
